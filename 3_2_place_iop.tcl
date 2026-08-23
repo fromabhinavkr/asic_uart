69139d9e23d98aea2f4d9949ffdca0a06ef3eb70
Abhinav K R <103116512+fromabhinavkr@users.noreply.github.com>
2026-08-24 03:23:44 +0530

    openroad results
    SkyWater SKY130 130nm process node. SkyWater Open Source PDK (Process Design Kit), specifically the sky130hd (High-Density) standard cell library.

---
+++ 3_2_place_iop.tcl
@@ -0,0 +1,16 @@
+#Edge: BOTTOM
+place_pin -pin_name btnd -layer met2 -location {265.19 0.242} -force_to_die_boundary
+place_pin -pin_name btnu -layer met2 -location {247.71 0.242} -force_to_die_boundary
+place_pin -pin_name led[0] -layer met2 -location {276.23 0.242} -force_to_die_boundary
+place_pin -pin_name led[1] -layer met2 -location {274.39 0.242} -force_to_die_boundary
+place_pin -pin_name led[2] -layer met2 -location {273.47 0.242} -force_to_die_boundary
+place_pin -pin_name led[3] -layer met2 -location {275.31 0.242} -force_to_die_boundary
+#Edge: RIGHT
+#Edge: TOP
+place_pin -pin_name tx -layer met2 -location {344.31 399.757} -force_to_die_boundary
+#Edge: LEFT
+place_pin -pin_name clk -layer met3 -location {0.4 372.98} -force_to_die_boundary
+place_pin -pin_name dht_in -layer met3 -location {0.4 100.98} -force_to_die_boundary
+place_pin -pin_name dht_oe -layer met3 -location {0.4 105.06} -force_to_die_boundary
+place_pin -pin_name dht_out -layer met3 -location {0.4 92.82} -force_to_die_boundary
+place_pin -pin_name rst -layer met3 -location {0.4 125.46} -force_to_die_boundary
